74197924b1076ae981b4bc648b92b73f91361361
203 <203@academicos.cesde.edu.co>
2022-02-16 10:04:50 -0500

    se crea controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,32 @@
+//ENTRADAS DEL PROBELMA (comentario de liena)
+
+/*
+Esto es
+un 
+Comentario de bloque
+*/
+//ENTRADAS==VARIABLES==CONSTANTES
+let nombreproducto="Camisetas de Tupac Shakur"
+let precioProducto=80000
+let pesoProducto=18.9
+let tieneDescuento=true
+let descricionProducto="Camisetas de la Linea  Tupac Shakur"
+let telefonoVendedor="3215745296"
+
+const NOMBRE_CLIENTE="Adrian"
+
+//Arreglos unidimensionales (vectores) 
+//Sirven para almacenar varios elementos en 1 sola variable
+
+let productos=["Camisetas, Pantalones, Pañoletas"]
+let productos2= Array("Camiseta, Pantalones, Pañoletas, Juana")
+
+
+//SALIDAS 
+console.log("Mi perro")
+console.log(nombreproducto)
+console.log("su nombre es: "+NOMBRE_CLIENTE+" y su producto es: "+nombreproducto)
+console.log(`su nombrer es: ${NOMBRE_CLIENTE} y su producto es ${nombreproducto}`)
+console.log(productos)
+console.log(productos2[2])
+
